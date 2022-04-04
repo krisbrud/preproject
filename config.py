72cf7fc8b04a2b42b574e68baab19b74fed5c1dc
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-04-04 11:44:24 +0200

    WIP on config setup

---
+++ config.py
@@ -0,0 +1,88 @@
+# my_project/config.py
+import os
+from yacs.config import CfgNode as CN
+
+
+_C = CN()
+
+## EXPERIMENT CONFIG
+_C.experiment = CN()
+# Name of experiment for naming tensorboard logs and runs in MLFlow
+_C.experiment.name = "example_experiment_name"
+
+## SYSTEM CONFIG
+_C.system = CN()
+# Log path. Required to be "./logs" by AzureML
+_C.system.log_path = os.path.join(os.curdir, "logs")
+# Path for outputs. Required to be "./outputs" by AzureML for saving of artifacts
+_C.system.output_path = os.path.join(os.curdir, "outputs")
+
+## TRAINING CONFIG
+_C.train = CN()
+# Number of parallel environments to use with SubProcVecEnv
+_C.train.num_envs = 8
+# Total timesteps to run training
+_C.train.total_timesteps = int(30e6)
+# How many timesteps between each time agent is saved to disk and MLFlow
+_C.train.save_freq = int(100e3)
+# MLFlow Tracking URI for logging metrics and artifacts. 
+# Set to None if it's not going to be used.
+_C.train.mlflow_tracking_uri
+# RL Algorithm to use. Currently supports "AssistedPPO", which is implmented in `krisbrud/assisted-baselines`.
+_C.train.algorithm = "AssistedPPO"
+# How many evaluation episodes to run when evaluating the environment
+_C.train.n_eval_episodes
+
+## ALGORITHM HYPERPARAMETERS
+# Hyperparameters for PPO algorithm
+_C.hyperparam = CN()
+# Also see https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html#parameters
+# for more details
+_C.hyperparam.n_steps = 1024
+_C.hyperparam.learning_rate = 1e-3
+_C.hyperparam.batch_size = 1024
+_C.hyperparam.gae_lambda = 0.95
+_C.hyperparam.gamma = 0.999 # Discount factor
+_C.hyperparam.n_epochs = 4  # Number of epochs per rollout
+_C.hyperparam.clip_range = 0.2  # Clip range for PPO objective function
+_C.hyperparam.ent_coef = 0.01   # Coefficient for entropy loss
+_C.hyperparam.verbose = 2
+
+## ASSISTANCE CONFIG
+# Configuration for assistants, masks and schedules
+_C.assistance = CN()
+_C.assistance.masks = CN()
+# Define masks
+# `assistant_control` and `agent_control` are NOT config variables, but
+# define the literals that the mask uses.
+assistant_control = True 
+agent_control = False
+# _C.assistance.masks.surge_only = 
+# (surge, rudder, elevator)
+[use_agent,        use_assistant,     use_assistant]
+[use_agent,        use_agent,         use_assistant]
+[use_agent,        use_agent,         use_agent]
+[use_agent,        use_assistant,     use_agent]
+[use_assistant,    use_agent,         use_assistant]
+[use_assistant,    use_assistant,     use_agent]
+[use_assistant,    use_agent,         use_agent]
+[use_assistant,    use_assistant,     use_assistant]
+
+## ENVIRONMENT CONFIG
+_C.env = CN()
+# Name of gym environment to look up
+_C.env.name = "PathFollowAuv3D-v0"
+
+
+
+
+
+def get_cfg_defaults():
+  """Get a yacs CfgNode object with default values for my_project."""
+  # Return a clone so that the defaults will not be altered
+  # This is for the "local variable" use pattern
+  return _C.clone()
+
+# Alternatively, provide a way to import the defaults as
+# a global singleton:
+# cfg = _C  # users can `from config import cfg`
