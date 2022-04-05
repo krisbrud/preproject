ffe8e4544e4f305a05967b43628ba71c8cad883a
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-04-05 11:06:55 +0200

    WIP on train3d

--- train3d.py
+++ train3d.py
@@ -5,19 +5,26 @@ import matplotlib as plt
 from utils.parsing import parse_experiment_info
 from utils.training_schemes import train_end_to_end_path_follow
 
+from config import get_cfg_defaults
+
+def get_config(verbose=True):
+    cfg = get_cfg_defaults()
+    cfg.merge_from_file("first-experiment.yaml") # Needs to be updated here if using different experiment config
+    cfg.freeze()
+
+    if verbose:
+        print("Config:\n", cfg)
+    
+    return cfg
+
+
 def main():
-    experiment_config = parse_experiment_info()
-
-    n_repetitions = experiment_config.n_repetitions 
+    cfg = get_config()
 
 
-    env_name = "PathFollowAuv3D-v0"
-    # env_name = "Track2d-v0"
-    for i in range(n_repetitions):
-        if n_repetitions > 1:
-            print(f"Experiment repetition {i} started!")
+    env_name = cfg.env.name
         
-        train_end_to_end_path_follow(hyperparams=hyperparams,
+    train_end_to_end_path_follow(hyperparams=hyperparams,
                                  experiment_config=experiment_config,
                                  env_name=env_name)
         
