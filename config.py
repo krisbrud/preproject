4c5e44d092e40b10928cbdb02df60093be3d7e19
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-04-18 16:40:10 +0200

    More work on config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # my_project/config.py
 import os
-# from yacs.config import CfgNode as CN
+
 from dataclasses import dataclass
 
 from assisted_baselines.common.assistants.pid import PIDGains
@@ -10,12 +10,14 @@ from assisted_baselines.common.assistants.pid import PIDGains
 class ExperimentConfig:
     name: str = "example_experiment_name"
 
+
 @dataclass
 class SystemConfig:
     # Log path. Required to be "./logs" by AzureML
     log_path: str = os.path.join(os.curdir, "logs")
     # Path for outputs. Required to be "./outputs" by AzureML for saving of artifacts
     output_path: str = os.path.join(os.curdir, "outputs")
+
 
 @dataclass
 class HyperparamConfig:
@@ -25,10 +27,10 @@ class HyperparamConfig:
     learning_rate: float = 1e-3
     batch_size: int = 1024
     gae_lambda: float = 0.95
-    gamma: float = 0.999 # Discount factor
+    gamma: float = 0.999  # Discount factor
     n_epochs = 4  # Number of epochs per rollout
     clip_range: float = 0.2  # Clip range for PPO objective function
-    ent_coef: float= 0.01   # Coefficient for entropy loss
+    ent_coef: float = 0.01  # Coefficient for entropy loss
     verbose: int = 2
 
 
@@ -37,10 +39,10 @@ class TrainConfig:
     # Number of parallel environments to use with SubProcVecEnv
     num_envs = 8
     # Total timesteps to run training
-    total_timesteps = int(100e3) # int(30e6)
+    total_timesteps = int(100e3)  # int(30e6)
     # How many timesteps between each time agent is saved to disk and MLFlow
     save_freq = int(100e3)
-    # MLFlow Tracking URI for logging metrics and artifacts. 
+    # MLFlow Tracking URI for logging metrics and artifacts.
     # Set to None if it's not going to be used.
     mlflow_tracking_uri = "azureml://northeurope.api.azureml.ms/mlflow/v1.0/subscriptions/3165a1c1-fd45-4c8d-938e-0058c823f960/resourceGroups/aml-playground/providers/Microsoft.MachineLearningServices/workspaces/aml-playground"
     # RL Algorithm to use. Currently supports "AssistedPPO", which is implmented in `krisbrud/assisted-baselines`.
@@ -48,18 +50,21 @@ class TrainConfig:
     # How many evaluation episodes to run when evaluating the environment
     n_eval_episodes = 100
 
+
 @dataclass
 class AuvPidConfig:
     # Gains for PID controllers for assistant
     # TODO
-    surge = PIDGains()
-    rudder = PIDGains()
-    elevator = PIDGains()
+    surge = PIDGains(Kp=2, Ki=1.5, Kd=0)
+    rudder = PIDGains(Kp=3.5, Ki=0.05, Kd=0.03)
+    elevator = PIDGains(Kp=3.5, Ki=0.05, Kd=0.03)
+
 
 @dataclass
 class AssistanceConfig:
     # Define parameters for PID controllers
     auv_pid: AuvPidConfig = AuvPidConfig()
+
 
 @dataclass
 class EnvConfig:
@@ -69,6 +74,7 @@ class EnvConfig:
     # Number of actuators in action space
     n_actions = 3
 
+
 @dataclass
 class Config:
     experiment: ExperimentConfig = ExperimentConfig()
@@ -77,8 +83,9 @@ class Config:
     train: TrainConfig = TrainConfig()
     assistance: AssistanceConfig = AssistanceConfig()
 
+
 def get_default_config() -> Config:
-    # Done by instantiating the config. 
+    # Done by instantiating the config.
     # Implemented as function for verbosity.
     config = Config()
     return config
--- utils/auv_masks.py
+++ utils/auv_masks.py
@@ -1,16 +1,16 @@
-AGENT_FLAG = True 
+AGENT_FLAG = True
 ASSISTANT_FLAG = False
 # Define masks
 # Masks are named after the actuators that are controlled by the RL agent
-# (surge, rudder, elevator)
 
 # TODO: Find out whether to instantiate the masks or pass the same ones
 
-mask_surge_only =                [AGENT_FLAG,        ASSISTANT_FLAG,     ASSISTANT_FLAG]
-mask_surge_and_rudder =          [AGENT_FLAG,        AGENT_FLAG,         ASSISTANT_FLAG]
-mask_agent_only =                [AGENT_FLAG,        AGENT_FLAG,         AGENT_FLAG]
-mask_surge_and_elevator =        [AGENT_FLAG,        ASSISTANT_FLAG,     AGENT_FLAG]
-mask_rudder_only =               [ASSISTANT_FLAG,    AGENT_FLAG,         ASSISTANT_FLAG]
-mask_elevator_only =             [ASSISTANT_FLAG,    ASSISTANT_FLAG,     AGENT_FLAG]
-mask_rudder_and_elevator =       [ASSISTANT_FLAG,    AGENT_FLAG,         AGENT_FLAG]
-mask_assistant_only =            [ASSISTANT_FLAG,    ASSISTANT_FLAG,     ASSISTANT_FLAG]
+# (surge, rudder, elevator)
+mask_surge_only = [AGENT_FLAG, ASSISTANT_FLAG, ASSISTANT_FLAG]
+mask_surge_and_rudder = [AGENT_FLAG, AGENT_FLAG, ASSISTANT_FLAG]
+mask_agent_only = [AGENT_FLAG, AGENT_FLAG, AGENT_FLAG]
+mask_surge_and_elevator = [AGENT_FLAG, ASSISTANT_FLAG, AGENT_FLAG]
+mask_rudder_only = [ASSISTANT_FLAG, AGENT_FLAG, ASSISTANT_FLAG]
+mask_elevator_only = [ASSISTANT_FLAG, ASSISTANT_FLAG, AGENT_FLAG]
+mask_rudder_and_elevator = [ASSISTANT_FLAG, AGENT_FLAG, AGENT_FLAG]
+mask_assistant_only = [ASSISTANT_FLAG, ASSISTANT_FLAG, ASSISTANT_FLAG]
